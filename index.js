7f4cc18d185af2729bc1cd6550a19d4f22a83776
Daramiry <drz0224@gmail.com>
2022-07-21 20:03:22 -0700

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your Code Here
+let answer  = window.prompt('Do you head left, or right?')
+let firstAnser = window.prompt('Do you head left, or right')
+
+if(firstAnswer === 'left') {
+    let secondAnswer = window.prompt ('You come across a stray cat. It scampers off down a small hole, just large enough for you to crawl through. Do you follow it, or continue on yor path')
+} else if (firstAnswer === 'right') {
+    let secontAnswer = window.prompt('You come across a snoring dragon.On the otheer side of him, you see a shiny chest of treasure. Another path would lead you away from the dragon all together. Which do you take?')
+}
+
+function start(){
+    let firstAnswer  = window.prompt('Do you head left, or right?')
+    if(firstAnswer === 'left'){
+       goLeft()
+    } else if(firstAnswer === 'right') {
+        goRight()
+    }
+}
+
+function goLeft(){
+    let secondAnswer = window.prompt(`You come across a stray cat. 
+    It scampers off down a small hole, just large enough for you to crawl through. 
+    Do you follow it, or continue on your path?`)
+    if(secondAnswer === 'back'){
+        start()
+    }
+}
+
+function goRight(){
+    let secondAnswer = window.prompt(`You come across a snoring dragon. 
+    On the other side of him, you see a shiny chest of treasure. Another path would 
+    lead you away from the dragon all together. Which do you take?`)
+    if(secondAnswer === 'back'){
+        start()
+    }
+}
+
+start()
